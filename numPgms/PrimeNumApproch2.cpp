d853afd746832b38d3de0fa1b62843d063d2c28d
Sushant Shendage <sushantshendage44@gmail.com>
2024-10-06 15:07:48 +0530

    cppPrimenNumberApproch2

--- numPgms/PrimeNumApproch2.cpp
+++ numPgms/PrimeNumApproch2.cpp
@@ -1,14 +1,14 @@
-//pgm_no:01
+//pgm_no:02
 //title:prime number( approch 1)
 /*
- 
+ (num/2) iteration
 */
 #include<iostream>
 using namespace std;
 int main()
 {
     int num=7,i;
- for (i = 2; i < num; i++)
+ for (i = 2; i <=( num/2); i++)
     {
         if (num % i == 0)
         {
@@ -16,7 +16,7 @@ int main()
         }
     }
 
-    if (i == num)
+    if (i == (num/2)+1)
     {
         std::cout<<num<<" is a prime number"<<endl;
     }
